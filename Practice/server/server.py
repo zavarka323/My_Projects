7efe5ade639ae155375277ce8a81f4dc456d505a
zavarka323 <100013756+zavarka323@users.noreply.github.com>
2022-05-28 22:04:11 +0300

    Update server.py

--- Practice/server/server.py
+++ Practice/server/server.py
@@ -6,15 +6,15 @@ import threading
 
 class Server:
     def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
-        self.all_client = []
+        self.ip = ip # IP Сервера
+        self.port = port # Port Сервера
+        self.all_client = [] # База пользователей чата
         
         # Запускаем прослушивание соединений
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.ip, self.port))
-        self.server.listen(0)
-        threading.Thread(target=self.connect_handler).start()
+        self.server.bind((self.ip, self.port)) # Начала прослушование по определенному ip порту
+        self.server.listen(0) # Максимальное количество пользовотелей, которое может обслуживать сервер (При значение "0", пользователей неограниченное количество)
+        threading.Thread(target=self.connect_handler).start() # Запуск потока для отслеживания новых соеденений 
         print('Сервер запущен!')
         
 
@@ -22,29 +22,29 @@ class Server:
     def connect_handler(self):
         while True:
             client, address = self.server.accept()
-            if client not in self.all_client:
-                self.all_client.append(client)
-                threading.Thread(target=self.message_handler, args=(client,)).start()
+            if client not in self.all_client: # Проверка пользователя на наличие его в чате
+                self.all_client.append(client) 
+                threading.Thread(target=self.message_handler, args=(client,)).start() # Запуск потока для обработки сообщений 
                 client.send('Успешное подключение к чату!'.encode('utf-8'))
             time.sleep(1)
 
 
-    # Обрабатываем отправленный текст
+    # Обрабатывание отправленного текста
     def message_handler(self, client_socket):
         while True:
             message = client_socket.recv(1024)
             print(message)
 
-            # Удаляем текущий сокет
-            if message == b'exit':
+            # Удаление текущего сокета если пользователь напишет "exit"
+            if message == b'exit': 
                 self.all_client.remove(client_socket)
                 break
 
-            for client in self.all_client:
+            for client in self.all_client: # Отправка сообщений другим пользователям 
                 if client != client_socket:
                     client.send(message)
             time.sleep(1)
             
 
 
-myserver = Server('127.0.0.1', 5555) 
+myserver = Server('127.0.0.1', 5555) # Создание IP и Port
